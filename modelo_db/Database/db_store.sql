597faa0a18b9b8e0a81eeeb858374977ca38ca71
ByDiegreeN <diegoalejandrogarcia140902@gmail.com>
2023-09-20 20:13:39 -0500

    Se hicieron cambios al script de la base de datos

--- modelo_db/Database/db_store.sql
+++ modelo_db/Database/db_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-08-2023 a las 16:53:05
+-- Tiempo de generación: 21-09-2023 a las 03:12:43
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,62 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_store`
 --
-
-DELIMITER $$
---
--- Procedimientos
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_delete_message` (IN `id` INT)   begin
-	delete from  tbl_message where message_id = id;
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_delete_products` (IN `id` INT)   begin
-
-delete from tbl_products where prod_id = id;
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_delete_users` (IN `id` INT)   begin
-	delete from  tbl_users where user_id = id;
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insert_message` (IN `msg_name` VARCHAR(100), IN `msg_asunto` VARCHAR(100), IN `msg_email` VARCHAR(150), IN `msg_text` VARCHAR(300))   begin
-	insert into tbl_message(message_name, message_asunto, message_email, message_m) values (msg_name, msg_asunto, msg_email, msg_text);
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insert_products` (IN `prod_img` VARCHAR(300), IN `prod_name` VARCHAR(80), IN `prod_desc` VARCHAR(300), IN `prod_price` INT, IN `prod_stock` INT)   begin
-	insert into tbl_products (prod_img, prod_name, prod_desc, prod_price, prod_stock) values (prod_img, prod_name, prod_desc, prod_price, prod_stock);
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insert_users` (IN `username` VARCHAR(80), `pass` VARCHAR(100), `ident` INT, `realname` VARCHAR(200), `email` VARCHAR(200), `phone` VARCHAR(15), `address` VARCHAR(80))   begin
-	insert into tbl_users(user_username, user_password, user_ident, user_realname, user_email, user_phonenumber, user_address) values (username, pass, ident, realname, email, phone, address);
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_update_products` (IN `id` INT, IN `prod_img` VARCHAR(300), IN `prod_name` VARCHAR(80), IN `prod_desc` VARCHAR(300), IN `prod_price` INT, IN `prod_stock` INT)   begin
-	update tbl_products set 
-    prod_img = prod_img,
-    prod_name = prod_name,
-	prod_desc = prod_desc,
-	prod_price = prod_price,
-	prod_stock = prod_stock
-                            
-	where prod_id = id;
-                            
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_update_users` (IN `id` INT, IN `username` VARCHAR(80), IN `pass` VARCHAR(100), IN `ident` INT, IN `realname` VARCHAR(200), IN `email` VARCHAR(200), IN `phonenumber` VARCHAR(15), `address` VARCHAR(80))   begin
-	update tbl_users set 
-    user_username = username,
-    user_password = pass,
-    user_ident = ident,
-    user_realname = realname,
-    user_email = email,
-    user_phonenumber = phonenumber,
-    user_address = address
-    
-    where user_id = id;
-end$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -107,6 +51,19 @@ CREATE TABLE `tbl_message` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tbl_notes`
+--
+
+CREATE TABLE `tbl_notes` (
+  `note_id` int(11) NOT NULL,
+  `note_tittle` varchar(256) NOT NULL,
+  `note_text` varchar(400) NOT NULL,
+  `note_created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tbl_products`
 --
 
@@ -114,9 +71,11 @@ CREATE TABLE `tbl_products` (
   `prod_id` int(11) NOT NULL,
   `prod_img` varchar(300) DEFAULT NULL,
   `prod_name` varchar(80) NOT NULL,
-  `prod_desc` varchar(300) DEFAULT NULL,
+  `prod_desc` varchar(300) DEFAULT 'No se ha proporcionado información',
   `prod_price` int(11) DEFAULT NULL,
-  `prod_stock` int(11) DEFAULT NULL,
+  `prod_stock` int(11) DEFAULT 1,
+  `prod_estado` varchar(50) DEFAULT 'Activo',
+  `prod_categoria` varchar(100) DEFAULT NULL,
   `prod_created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -134,17 +93,17 @@ CREATE TABLE `tbl_users` (
   `user_realname` varchar(200) NOT NULL,
   `user_email` varchar(200) NOT NULL,
   `user_phonenumber` varchar(15) NOT NULL,
-  `user_address` varchar(80) DEFAULT NULL,
+  `user_address` varchar(80) DEFAULT 'No se ha agregado una dirección',
   `user_created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_users_products`
+-- Estructura de tabla para la tabla `tbl_users_cart`
 --
 
-CREATE TABLE `tbl_users_products` (
+CREATE TABLE `tbl_users_cart` (
   `prod_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -157,6 +116,8 @@ CREATE TABLE `tbl_users_products` (
 
 CREATE TABLE `tbl_venta` (
   `venta_id` int(11) NOT NULL,
+  `venta_cantidad` int(11) DEFAULT NULL,
+  `venta_precio` int(11) DEFAULT NULL,
   `prod_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `venta_created_at` timestamp NOT NULL DEFAULT current_timestamp()
@@ -179,6 +140,12 @@ ALTER TABLE `tbl_message`
   ADD PRIMARY KEY (`message_id`);
 
 --
+-- Indices de la tabla `tbl_notes`
+--
+ALTER TABLE `tbl_notes`
+  ADD PRIMARY KEY (`note_id`);
+
+--
 -- Indices de la tabla `tbl_products`
 --
 ALTER TABLE `tbl_products`
@@ -188,12 +155,13 @@ ALTER TABLE `tbl_products`
 -- Indices de la tabla `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `CheckUniqueEmail` (`user_email`);
 
 --
--- Indices de la tabla `tbl_users_products`
+-- Indices de la tabla `tbl_users_cart`
 --
-ALTER TABLE `tbl_users_products`
+ALTER TABLE `tbl_users_cart`
   ADD KEY `user_id` (`user_id`),
   ADD KEY `prod_id` (`prod_id`);
 
@@ -222,6 +190,12 @@ ALTER TABLE `tbl_message`
   MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `tbl_notes`
+--
+ALTER TABLE `tbl_notes`
+  MODIFY `note_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `tbl_products`
 --
 ALTER TABLE `tbl_products`
@@ -244,12 +218,11 @@ ALTER TABLE `tbl_venta`
 --
 
 --
--- Filtros para la tabla `tbl_users_products`
+-- Filtros para la tabla `tbl_users_cart`
 --
-ALTER TABLE `tbl_users_products`
-  ADD CONSTRAINT `tbl_users_products_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`user_id`),
-  ADD CONSTRAINT `tbl_users_products_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`user_id`),
-  ADD CONSTRAINT `tbl_users_products_ibfk_3` FOREIGN KEY (`prod_id`) REFERENCES `tbl_products` (`prod_id`);
+ALTER TABLE `tbl_users_cart`
+  ADD CONSTRAINT `tbl_users_cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`user_id`),
+  ADD CONSTRAINT `tbl_users_cart_ibfk_2` FOREIGN KEY (`prod_id`) REFERENCES `tbl_products` (`prod_id`);
 
 --
 -- Filtros para la tabla `tbl_venta`
